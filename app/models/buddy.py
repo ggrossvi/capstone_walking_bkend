969dddd7d0f9d008ac05fa4b622da02dd5df8852
ggrossvi <hello.gloria@gmail.com>
2021-08-11 16:13:48 -0700

    added check buddy via email route

--- app/models/buddy.py
+++ app/models/buddy.py
@@ -3,8 +3,13 @@ from app import db
 
 class Buddy(db.Model):
     buddy_id = db.Column(db.Integer,autoincrement=True,primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    address=db.Column(db.Text)
+    apt=db.Column(db.Text)
+    city=db.Column(db.Text)
+    state=db.Column(db.Text)
     zipcode=db.Column(db.Text)
-    name = db.Column(db.Text)
     email = db.Column(db.Text)
     morning =db.Column(db.Boolean)
     afternoon =db.Column(db.Boolean)
@@ -12,9 +17,14 @@ class Buddy(db.Model):
 
     def to_json(self):
         return {
-            'user_id': self.buddy_id,
+            'buddy_id': self.buddy_id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'address': self.address,
+            'apt': self.apt,
+            'city':self.city,
+            'state':self.state,
             'zipcode': self.zipcode,
-            'name': self.name,
             'email': self.email,
             'morning':self.morning,
             'afternoon':self.afternoon,
--- app/route.py
+++ app/route.py
@@ -1,15 +1,19 @@
 from app.models.buddy import Buddy
 from flask import Blueprint, jsonify,request
-from app.models.buddy import Buddy
 from app import db
 
-buddy_bp = Blueprint("buddy", __name__, url_prefix='/buddy')
+buddy_bp = Blueprint("buddy_bp", __name__, url_prefix='/buddy')
 
 @buddy_bp.route("",methods=["POST"])
 def create_buddy():
     request_body = request.get_json()
     buddy = Buddy(
-        name = request_body["name"],
+        first_name = request_body["first_name"],
+        last_name = request_body["last_name"],
+        address = request_body["address"],
+        apt = request_body["apt"],
+        city = request_body["city"],
+        state = request_body["state"],
         zipcode = request_body["zipcode"],
         email = request_body["email"],
         morning = request_body["morning"],
@@ -32,6 +36,87 @@ def get_all_users():
             response.append(buddy.to_json())
         return jsonify(response),200
 
+@buddy_bp.route('/zip/<zip>', methods=["GET"])
+def get_all_users_zip(zip):
+
+    # get sort query param
+    
+    
+    buddies = Buddy.query.filter_by(zipcode=zip)
+    print("buddies",buddies)
+    print(buddies.count())
+    if buddies.count() == 0:
+        return jsonify({"No Buddies found!"},204)
+    else:
+        response = []
+        for buddy in buddies:
+            response.append(buddy.to_json())
+        return jsonify(response),200
+
+@buddy_bp.route('/email/<email>', methods=["GET"])
+def get_user_email(email):
+
+    # get sort query param
+    
+    
+    buddies = Buddy.query.filter_by(email=email)
+    print("buddies",buddies)
+    print(buddies.count())
+    if buddies.count() == 0:
+        return jsonify({"message": "User not found"},404)
+    else:
+        response = []
+        for buddy in buddies:
+            response.append(buddy.to_json())
+        return jsonify(response),200
+
+#Get Specific Buddy
+# @buddy_bp.route('/<buddy_id>', methods = ['GET','PUT', 'DELETE'])  
+# def handle_goal(buddy_id):
+#     buddy = Buddy.query.get(buddy_id)
+#     if not buddy:
+#         return "", 404
+
+#     if request.method == 'GET':
+#         #check expected output in test
+#         return({
+#             'goal':
+#                 goal.serialize()
+            
+#         })
+#     elif request.method == 'PUT':
+#         request_body = request.get_json()
+#         if 'title' in request_body:
+#             goal.title = request_body['title']
+#         db.session.commit()
+#         return({
+#             'goal': goal.serialize()
+#         },200)
+    
+#     elif request.method =='DELETE':
+#         db.session.delete(goal)
+#         db.session.commit()
+#         return({
+#             "details": f'Goal {goal_id} "{goal.title}" successfully deleted'
+#         },200)
+
+# make this zip === zip and morning === morning or afternoon === afternoon or evening === evening
+# @buddy_bp.route('/zip/<zip>', methods=["GET"])
+# def get_all_users_zip(zip):
+
+#     # get sort query param
+    
+    
+#     buddies = Buddy.query.filter_by(zipcode=zip)
+#     print("buddies",buddies)
+#     print(buddies.count())
+#     if buddies.count() == 0:
+#         return jsonify({"No Buddies found!"},204)
+#     else:
+#         response = []
+#         for buddy in buddies:
+#             response.append(buddy.to_json())
+#         return jsonify(response),200
 
 @buddy_bp.route('/JSON', methods=["GET"])
 def walking_json():
@@ -42,31 +127,33 @@ def walking_json():
     }, 200
 
 
-@buddy_bp.route('/<buddy_id>', methods = ['GET','PUT', 'DELETE'])  
-def handle_buddy(buddy_id):
-    buddy = Buddy.query.get(buddy_id)
-    if not buddy:
-        return "", 404
+# @buddy_bp.route('/<buddy_id>', methods = ['GET','PUT', 'DELETE'])  
+# def handle_buddy(buddy_id):
+#     buddy = Buddy.query.get(buddy_id)
+#     if not buddy:
+#         return "", 404
 
-    if request.method == 'GET':
-        #check expected output in test
-        return({
-            'buddy':
-                buddy.to_json()
+#     if request.method == 'GET':
+#         #check expected output in test
+#         return({
+#             'buddy':
+#                 buddy.to_json()
             
-        })
-    elif request.method == 'PUT':
-        request_body = request.get_json()
-        if 'name' in request_body:
-            buddy.name = request_body['name']
-        db.session.commit()
-        return({
-            'buddy': buddy.to_json()
-        },200)
+#         })
+#     elif request.method == 'PUT':
+#         # # ?????? What do I need to put in request body
+#         # request_body = request.get_json()
+#         # if 'name' in request_body:
+#         #     buddy.name = request_body['name']
+#         # db.session.commit()
+#         # return({
+#         #     'buddy': buddy.to_json()
+#         # },200)
+#         pass
     
-    elif request.method =='DELETE':
-        db.session.delete(buddy)
-        db.session.commit()
-        return({
-            "details": f'Buddy {buddy_id} "{buddy.name}" successfully deleted'
-        },200)
+#     elif request.method =='DELETE':
+#         db.session.delete(buddy)
+#         db.session.commit()
+#         return({
+#             "details": f'Buddy {buddy_id} "{buddy.name}" successfully deleted'
+#         },200)
