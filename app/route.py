93cf13d5bee52b416f0817bdb22d95d6cd5d79f2
ggrossvi <hello.gloria@gmail.com>
2021-08-15 12:16:02 -0700

    the put is working in postman if I give it and id, now need to get it from state

--- app/route.py
+++ app/route.py
@@ -91,52 +91,91 @@ def get_user_email(email):
         return jsonify(response),200
 
 #Get Specific Buddy
-# @buddy_bp.route('/<buddy_id>', methods = ['GET','PUT', 'DELETE'])  
-# def handle_goal(buddy_id):
-#     buddy = Buddy.query.get(buddy_id)
-#     if not buddy:
-#         return "", 404
+@buddy_bp.route('/<buddy_id>', methods = ['GET','PUT', 'DELETE'])  
+def update_buddy(buddy_id):
+    buddy = Buddy.query.get(buddy_id)
+    if not buddy:
+        return "", 404
 
-#     if request.method == 'GET':
-#         #check expected output in test
-#         return({
-#             'goal':
-#                 goal.serialize()
+    if request.method == 'GET':
+        #check expected output in test
+        return({
+            'buddy':
+                buddy.to_json()
             
-#         })
-#     elif request.method == 'PUT':
-#         request_body = request.get_json()
-#         if 'title' in request_body:
-#             goal.title = request_body['title']
-#         db.session.commit()
-#         return({
-#             'goal': goal.serialize()
-#         },200)
+        })
+    elif request.method == 'PUT':
+        request_body = request.get_json()
+        #### if for all the fields
+        if 'first_name' in request_body:
+            buddy.first_name = request_body['first_name']
+        if 'last_name' in request_body:
+           buddy.last_name = request_body['last_name']
+        if 'address' in request_body:
+           buddy.address = request_body['address']
+        if 'apt' in request_body:
+           buddy.apt= request_body['apt']
+        if 'city' in request_body:
+           buddy.city = request_body['city']
+        if 'state' in request_body:
+           buddy.state = request_body['state']
+        if 'zipcode' in request_body:
+           buddy.zipcode = request_body['zipcode']
+        if 'email' in request_body:
+           buddy.email = request_body['email']
+        if 'morning' in request_body:
+           buddy.morning = request_body['morning']
+        if 'afternoon' in request_body:
+           buddy.afternoon = request_body['afternoon']
+        if 'evening' in request_body:
+           buddy.evening = request_body['evening']
+        if 'bio' in request_body:
+           buddy.bio = request_body['bio']
+        
+           
+
+        db.session.commit()
+        return({
+            'buddy':
+                buddy.to_json()
+        },200)
     
-#     elif request.method =='DELETE':
-#         db.session.delete(goal)
-#         db.session.commit()
-#         return({
-#             "details": f'Goal {goal_id} "{goal.title}" successfully deleted'
-#         },200)
+    # elif request.method =='DELETE':
+    #     db.session.delete(goal)
+    #     db.session.commit()
+    #     return({
+    #         "details": f'Goal {goal_id} "{goal.title}" successfully deleted'
+    #     },200)
 
 # make this zip === zip and morning === morning or afternoon === afternoon or evening === evening
-# @buddy_bp.route('/zip/<zip>', methods=["GET"])
-# def get_all_users_zip(zip):
+@buddy_bp.route('/zip/<zip>/morning/<morning>/afternoon/<afternoon>/evening/<evening>', methods=["GET"])
+def get_all_users_zip_morning(zip, morning,afternoon,evening):
+    # zip = request.args.get('zip')
+    # morning = request.args.get('morning')
+    # get sort query param
+    print(zip,morning)
 
-#     # get sort query param
-    
-    
-#     buddies = Buddy.query.filter_by(zipcode=zip)
-#     print("buddies",buddies)
-#     print(buddies.count())
-#     if buddies.count() == 0:
-#         return jsonify({"No Buddies found!"},204)
-#     else:
-#         response = []
-#         for buddy in buddies:
-#             response.append(buddy.to_json())
-#         return jsonify(response),200
+
+    buddies = Buddy.query.filter_by(zipcode=zip)
+    # print("buddies",buddies)
+    # print(buddies.count())
+    if buddies.count() == 0:
+        return jsonify({"No Buddies found!"},204)
+    else:
+        response = []
+        for buddy in buddies:
+            print(buddy.morning)
+            print(morning)
+            if morning == "1":
+                if buddy.morning:
+                    response.append(buddy.to_json())
+            if afternoon == "1":
+                if buddy.afternoon:
+                    response.append(buddy.to_json())
+            if evening == "1":
+                if buddy.evening:
+                    response.append(buddy.to_json())
+        return jsonify(response),200
 
 @buddy_bp.route('/JSON', methods=["GET"])
 def walking_json():
