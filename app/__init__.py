c772ebf635a24ed18bbfc440a791f038a3a36eb9
ggrossvi <hello.gloria@gmail.com>
2021-08-16 17:50:28 -0700

    hide env variables

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config = None):
     CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@localhost:5432/walkingbuddy'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     
 
     db.init_app(app)
